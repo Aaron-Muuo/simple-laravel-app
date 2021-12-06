117ca5c1452928aa5e9de35e564ecc91ad7a7d76
Aaron-Muuo <aaronmuuo60@gmail.com>
2021-12-06 16:18:45 +0300

    Laravel simple blog site

---
+++ resources/views/home.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.master')
+@section('pageTitle', 'Home')
+
+@section('content')
+<div class="row mt-5">
+    <div class="row">
+        <div class="col-md-9">
+        <h3>THE GUARDIANS OF LORE</h3>
+        <p class="text-muted">Online stories, posted by <span class="text-danger">Aaron</span></p>
+        
+        <div id="1" class="mt-3">
+            <h4>1. BOY MEETS FROG</h4>
+            <p>I was sitting in class the first time that I saw the frog.
+            <br>
+            Miss Weaver had been my teacher for a few months, and was best known around Stagwood for having a stack of black hair that rose a foot above her head. Before the school year started, I’d heard rumors about her, and within a week I realized that they were all true. For one thing, she did, in fact, wear the same outfit every day; the colors changed, but she always had on striped pants and a striped jacket. For another thing, she was mind-numbingly boring. The kind of boring that makes your eyes shut without your permission. The biggest problem, though, was the stories. She was obsessed with tales of former students who had become some kind of famous. The first couple of times weren’t bad, maybe even kind of interesting, but by the second week of school she had started repeating herself, just like her outfits. </p>
+        </div>
+
+        <div id="2" class="mt-3">
+            <h4>2. SOY</h4>
+            <p>I didn’t see the frog for the rest of the day. Maybe it stayed in Miss Weaver’s hair or maybe it jumped back through the window when I wasn’t looking. All day long, I expected her to find it and scream, but she never did. <br>When the bell rang, I ran out of the school onto the side of the yard with bushes. I had seen frogs hide in there before, but there was no sign of it. Truthfully, I wouldn’t have known what to do with the frog if I had found it. Would I capture it? Would I ask it questions? If I was caught talking to a frog outside the school, I think people might finally start to remember me, but in a #donttalktothefrogkid kind of way. <br>I liked most of the kids in my class just fine, but I was only really close with one. His name was Soy, and he was definitely, without a doubt, my best friend. Soy had kind of reddish-blond hair and a round face. He and I had been friends since preschool, and I knew that I could talk to him about anything. The downside to that was that he also felt he could talk to me about anything. Since he had eight older brothers and two older sisters, his questions were always about things that I didn’t understand.</p>
+        </div>
+        <div id="3" class="mt-3">
+            <h4>3. DAYDREAMS</h4>
+            <p>Soy had the frog in his hands within seconds. He was always good with outdoorsy stuff like that. His mom was usually busy with his siblings, so Soy was left to play outside a lot. By third grade, he had caught a pet turtle, salamander, and flying squirrel. Unfortunately, his older brothers Rick and Hank stole the squirrel and kept it in their room. Once a week they played a game where Rick threw the squirrel from the second story window, and Hank caught it midair with a trash can as it glided down. Soy tried to get them to stop, but they never listened. <br><br>The frog didn’t put up much of a fight. I started to wonder if it actually wanted to be caught. <br>“Do you believe me now?” I asked. Soy peaked inside his hands, and then lifted his head. "Well… this frog is wearing glasses. I’ll give you that.” Both of our minds were racing with questions. “Let’s bring it to my treehouse, so we can figure this out,” I said. </p>
+        </div>
+        <div id="3" class="mt-3">
+         
+            <p>Get more stories <a href="https://www.freechildrenstories.com/guardians-of-lore" class="text-danger">here.</a></p>
+        </div>
+
+        </div>
+
+        <div class="col-md-3">
+            <div class="list-group">
+                <a href="#1" class="list-group-item list-group-item-action">Chapter One: Boy Meets Frog</a>
+                <a href="#2" class="list-group-item list-group-item-action">Chapter Two: Soy</a>
+                <a href="#3" class="list-group-item list-group-item-action">Chapter Three: Daydreams</a>
+                </div>
+            </div>
+    </div>
+</div>
+@endsection
+
+@section('js')
+
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Blog app | @yield('pageTitle')</title>
+
+        <!-- Fonts -->
+        <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet">
+        <link href="{{asset('css/all.css')}}" rel="stylesheet">
+        <link href="{{asset('css/stylesheet.css')}}" rel="stylesheet">
+
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row mt-5">
+                <div class="col-md-12">
+                    <p class="text-muted text-center m-0">Welcome</p>
+                    <h3 class="fw-bold fs-4 text-center">My simple web-application</h3>
+                    <p class="fw-bold text-center">Built with Laravel</p>
+                    
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div class="container">
+                        <a class="navbar-brand" href="http://github.com/Aaron-Muuo" >A.M</a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item">
+                            <a href="/" class="{{ (request()->is('/') || request()-> is('home')) ? 'nav-link active' : 'nav-link' }}">Home</a>
+                            </li>
+                            <li class="nav-item">
+                            <a href="/blog" class="{{ (request()-> is('blog')) ? 'nav-link active' : 'nav-link' }}">Blog</a>
+                            </li>
+                            <li class="nav-item">
+                            <a href="/media" class="{{ (request()-> is('media')) ? 'nav-link active' : 'nav-link' }}">Media</a>
+                            </li>
+                            <li class="nav-item">
+                            <a href="/signin" class="{{ (request()-> is('signin')) ? 'nav-link active' : 'nav-link' }}">Sign in</a>
+                            </li>
+                        </ul>
+                        </div>
+                    </div>
+                    </nav>
+                </div>
+            </div>
+
+            @yield('content')
+
+        </div>
+
+        {{-- footer section --}}
+        <div class="bg-dark">
+            <div class="container">
+                <div class="row">
+                <div class="col-md-12 text-center">
+                <p class="text-light mt-3">Copyright 2021. Made by Aaron. Follow me on <a href="http://github.com/Aaron-Muuo" class="text-danger" data-bs-toggle="tooltip" data-bs-placement="top" title="Github: Aaron-Muuo" style="text-decoration: none">github.</a></p>
+                </div>
+                </div>
+            </div>
+        </div>
+      {{-- end of footer --}}
+       
+        @yield('js')
+
+        <script src="{{asset('js/jquery-3.3.1.js')}}"></script>
+        <script src="{{asset('js/popper.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap.js')}}"></script>
+
+        <script src="{{asset('js/master.js')}}"></script>
+
+    </body>
+</html>
